bbf4f3a76de531100fa84891e98e41fa3513f846
David Marchante <davidmarchan@gmail.com>
2018-05-30 12:00:10 +0200

    Public component init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { PublicComponent } from './public/public.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/public', pathMatch: 'full' },
+  { path: 'public', component: PublicComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
