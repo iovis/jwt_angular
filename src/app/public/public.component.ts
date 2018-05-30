924cb8f812ebf855522ec0a9aac7cd5d1addd392
David Marchante <davidmarchan@gmail.com>
2018-05-30 12:25:43 +0200

    Consume /public in the Public component

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../api.service';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-public',
   templateUrl: './public.component.html',
   styleUrls: ['./public.component.css']
 })
 export class PublicComponent implements OnInit {
+  public content: Observable<string>;
 
-  constructor() { }
+  constructor(
+    private api: ApiService
+  ) { }
 
   ngOnInit() {
+    this.content = this.api.getPublic();
   }
-
 }
