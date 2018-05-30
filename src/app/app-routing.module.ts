46c1e3f6ef61915f84e626cd1e7aeda4ca85189c
David Marchante <davidmarchan@gmail.com>
2018-05-30 12:39:47 +0200

    Create navbar routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { PublicComponent } from './public/public.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/public', pathMatch: 'full' },
-  { path: 'public', component: PublicComponent }
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'public', component: PublicComponent },
+  { path: 'login', component: LoginComponent }
 ];
 
 @NgModule({
